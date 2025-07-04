6b3a18fc459ebc9db2f5627b8822f73fcd78cef9
hibaanadani <hibaanadani1@gmail.com>
2025-07-04 22:58:19 +0300

    update

--- article-server-main/seeders/categories_seeders.php
+++ article-server-main/seeders/categories_seeders.php
@@ -1,10 +1,27 @@
 <?php
 require_once __DIR__ . '/connections/connection.php';
-DB::connection("categories")->table('tblgeneral')->insert([
-$users = [
-        ['id' => '1', 'category_name' => 'Action', 'articles_id' => '[2,4,5,7,10]'],
-        ['id' => '2', 'category_name' => 'Romance', 'articles_id' => '[1,3,5,10]'],
-        ['id' => '3', 'category_name' => 'Horror', 'articles_id' => '[6,8,9]'],
+DB::connection("articles_db")->table('categories')->insert([
+$category = [
+        ['id' => '1', 'category_name' => 'Technology'],
+        ['id' => '2', 'category_name' => 'Sports'],
+        ['id' => '3', 'category_name' => 'News'],
+        ['id' => '4', 'category_name' => 'Health'],
+        ['id' => '5', 'category_name' => 'Travel'],
+        ['id' => '6', 'category_name' => 'Weather'],
+        ['id' => '7', 'category_name' => 'Food'],
+        ['id' => '8', 'category_name' => 'Fashion'],
+    ];
+]);
+DB::connection("articles_db")->table('article_category')->insert([
+$relation = [
+        ['article_id' => '1,5', 'category_id' => 'Technology'],
+        ['article_id' => '2,7', 'category_id' => 'Sports'],
+        ['article_id' => '3,6,8', 'category_id' => 'News'],
+        ['article_id' => '4,1,8', 'category_id' => 'Health'],
+        ['article_id' => '5,1,3,7', 'category_id' => 'Travel'],
+        ['article_id' => '6,5,4', 'category_id' => 'Weather'],
+        ['article_id' => '7,6,8', 'category_id' => 'Food'],
+        ['article_id' => '8,7,3', 'category_id' => 'Fashion'],
     ];
 ]);
     ?>
